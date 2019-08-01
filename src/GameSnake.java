f8dc944b02a09c6e5714ccbe80d33755947fc25a
snfnmeister <igorizhenbin@gmail.com>
2019-08-01 10:50:02 +0700

    updates

--- src/GameSnake.java
+++ src/GameSnake.java
@@ -9,4 +9,36 @@ import java.util.*;
 
 public class GameSnake {
 
+    //constants
+    final String TITLE_OF_PROGRAMM = "Classic Game Snake";
+    final String GAME_OVER_MSG =  "GAME OVER";
+    final int POINT_RADIUS = 20; // circle radius in px
+    final int FIELD_WIDTH = 30; // field size in POINT_RADIUS
+    final int FIELD_HEIGHT = 20; // field size in POINT_RADIUS
+    final int FIELD_DX = 6; //delimeters?
+    final int FIELD_DY = 28;
+    final int START_LOCATION = 200; // game start positions
+    final int START_SNAKE_SIZE = 6;
+    final int START_SNAKE_X = 10;
+    final int START_SNAKE_Y = 10;
+    final int SHOW_DELAY = 150; // in ms
+    final int LEFT= 37; //move left (in keyboards codes)
+    final int RIGHT = 39; //move right
+    final int UP = 38; // move UP
+    final int DOWN = 40; // move DOWN
+    final int START_DIRECTION = RIGHT; // snake start moving to right
+    final Color DEFAULT_COLOR = Color.black;
+    final Color FOOD_COLOR = Color.green;
+    final Color POISON_COLOR = Color.red;
+
+
+    public static void main (String[] args) {
+        new GameSnake().go();
+
+    }
+
+    void go() {
+
+    }
+
 }
